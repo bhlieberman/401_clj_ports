fb0b8c8b3657496ea5ce651d9811cda8654b83fd
blieberman@healthcareanalytics.expert <blieberman@healthcareanalytics.expert>
2022-11-04 07:12:08 -0600

    file writes from API successful

--- src/quotes/quotes.clj
+++ src/quotes/quotes.clj
@@ -1,10 +1,13 @@
 (ns quotes.quotes
-  (:require [clojure.test :refer [deftest is run-test]])
+  (:require [clojure.test :refer [deftest is run-test]]
+            [clojure.java.io :as io])
   (:import [java.net.http HttpClient HttpRequest
             HttpResponse$BodyHandlers]
-           [java.net URI]))
+           [java.net URI]
+           [java.io FileNotFoundException]))
 
-(def sys-data {:url "https://ron-swanson-quotes.herokuapp.com/v2/quotes"})
+(def sys-data {:api-url "https://ron-swanson-quotes.herokuapp.com/v2/quotes"
+               :fallback "resources/quotes.json"})
 
 (defn- build-req [url]
   (let [uri (URI. url)
@@ -19,16 +22,28 @@
 (defn read-resp [resp]
   (.body resp))
 
+(defn write-resp [body]
+  (with-open [writer (io/writer (:fallback sys-data) :append true)]
+    (.write writer body)
+    (catch FileNotFoundException)))
+
+(deftest test-write-resp-to-file
+  (is (not (instance? FileNotFoundException
+                  (as-> (build-req (:api-url sys-data)) req
+                    (->> (send-req req)
+                         read-resp
+                         write-resp))))))
+
 (deftest read-response
-  (as-> (build-req (:url sys-data)) req
+  (as-> (build-req (:api-url sys-data)) req
     (->> (send-req req)
          read-resp
          (is string?))))
 
 (deftest send-request
-  (is (= 200 (.statusCode (send-req (build-req (:url sys-data)))))))
+  (is (= 200 (.statusCode (send-req (build-req (:api-url sys-data)))))))
 
 (deftest build-request
-  (is (= 2 (count (build-req (:url sys-data))))))
+  (is (= 2 (count (build-req (:api-url sys-data))))))
 
-(run-test read-response)
+(run-test test-write-resp-to-file)
