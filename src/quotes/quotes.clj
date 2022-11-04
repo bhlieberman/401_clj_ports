ccd58c23247c97d2c6a206ed3ea2c5bc8c58ac55
blieberman@healthcareanalytics.expert <blieberman@healthcareanalytics.expert>
2022-11-04 12:57:31 -0600

    failover functionality added to API

--- src/quotes/quotes.clj
+++ src/quotes/quotes.clj
@@ -1,13 +1,21 @@
 (ns quotes.quotes
   (:require [clojure.test :refer [deftest is run-test]]
-            [clojure.java.io :as io])
+            [clojure.java.io :as io]
+            [portal.api :as p])
   (:import [java.net.http HttpClient HttpRequest
             HttpResponse$BodyHandlers]
            [java.net URI]
            [java.io FileNotFoundException]))
 
+(def p (p/open {:launcher :vs-code}))
+(add-tap #'p/submit)
+
 (def sys-data {:api-url "https://ron-swanson-quotes.herokuapp.com/v2/quotes"
                :fallback "resources/quotes.json"})
+
+(defn fallback-quote []
+  (with-open [rdr (io/reader "resources/quotes.json")]
+    (rand-nth (line-seq rdr))))
 
 (defn- build-req [url]
   (let [uri (URI. url)
@@ -17,22 +25,25 @@
 (defn send-req [req]
   (let [client (HttpClient/newHttpClient)
         handler (HttpResponse$BodyHandlers/ofString)]
-    (.send client req handler)))
+    (try (.send client req handler)
+         (catch Exception _
+           (fallback-quote)))))
 
 (defn read-resp [resp]
   (.body resp))
 
-(defn write-resp [body]
-  (with-open [writer (io/writer (:fallback sys-data) :append true)]
-    (.write writer body)
-    (catch FileNotFoundException)))
+(defn write-resp [body & {:keys [append]}]
+  (with-open [writer (io/writer (:fallback sys-data) :append append)]
+    (if append
+      (.write writer (str ",\n" body))
+      (.write writer body))))
 
 (deftest test-write-resp-to-file
   (is (not (instance? FileNotFoundException
-                  (as-> (build-req (:api-url sys-data)) req
-                    (->> (send-req req)
-                         read-resp
-                         write-resp))))))
+                      (as-> (build-req (:api-url sys-data)) req
+                        (-> (send-req req)
+                            read-resp
+                            (write-resp {:append true})))))))
 
 (deftest read-response
   (as-> (build-req (:api-url sys-data)) req
@@ -42,8 +53,5 @@
 
 (deftest send-request
   (is (= 200 (.statusCode (send-req (build-req (:api-url sys-data)))))))
-
-(deftest build-request
-  (is (= 2 (count (build-req (:api-url sys-data))))))
 
 (run-test test-write-resp-to-file)
